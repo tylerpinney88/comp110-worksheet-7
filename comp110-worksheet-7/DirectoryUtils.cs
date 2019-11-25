16d9d6ab4d0af77b3536992fc4590b1d6e7420d7
edpowley <edpowley@gmail.com>
2019-11-25 01:16:40 +0000

    Skeleton project

---
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp110_worksheet_7
+{
+	public static class DirectoryUtils
+	{
+		// Return the size, in bytes, of the given file
+		public static long GetFileSize(string filePath)
+		{
+			return new FileInfo(filePath).Length;
+		}
+
+		// Return true if the given path points to a directory, false if it points to a file
+		public static bool IsDirectory(string path)
+		{
+			return File.GetAttributes(path).HasFlag(FileAttributes.Directory);
+		}
+
+		// Return the total size, in bytes, of all the files below the given directory
+		public static long GetTotalSize(string directory)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Return the number of files (not counting directories) below the given directory
+		public static int CountFiles(string directory)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
+		public static int GetDepth(string directory)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Get the path and size (in bytes) of the smallest file below the given directory
+		public static Tuple<string, long> GetSmallestFile(string directory)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Get the path and size (in bytes) of the largest file below the given directory
+		public static Tuple<string, long> GetLargestFile(string directory)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Get all files whose size is equal to the given value (in bytes) below the given directory
+		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- comp110-worksheet-7/Program.cs
+++ comp110-worksheet-7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,66 @@ namespace comp110_worksheet_7
 	{
 		static void Main(string[] args)
 		{
+			const string c_baseDirectory = "../../../comp110-worksheet-7-tests/pixelcarpack_kenney/PNG";
+
+			try
+			{
+				Console.WriteLine("Total size: {0} bytes", DirectoryUtils.GetTotalSize(c_baseDirectory));
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("GetTotalSize is not implemented");
+			}
+
+			try
+			{
+				Console.WriteLine("Number of files: {0}", DirectoryUtils.CountFiles(c_baseDirectory));
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("CountFiles is not implemented");
+			}
+
+			try
+			{
+				Console.WriteLine("Folder depth: {0}", DirectoryUtils.GetDepth(c_baseDirectory));
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("GetDepth is not implemented");
+			}
+
+			try
+			{
+				var smallest = DirectoryUtils.GetSmallestFile(c_baseDirectory);
+				Console.WriteLine("Smallest file:\n  {0}\n  ({1} bytes)", smallest.Item1, smallest.Item2);
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("GetSmallestFile is not implemented");
+			}
+
+			try
+			{
+				var largest = DirectoryUtils.GetLargestFile(c_baseDirectory);
+				Console.WriteLine("Largest file:\n  {0}\n  ({1} bytes)", largest.Item1, largest.Item2);
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("GetLargestFile is not implemented");
+			}
+
+			try
+			{
+				Console.WriteLine("Files of size 253 bytes:");
+				foreach (string f in DirectoryUtils.GetFilesOfSize(c_baseDirectory, 253))
+					Console.WriteLine("   {0}", f);
+			}
+			catch (NotImplementedException)
+			{
+				Console.WriteLine("GetFilesOfSize is not implemented");
+			}
+
 			Console.WriteLine("Press enter to continue");
 			Console.ReadLine();
 		}
