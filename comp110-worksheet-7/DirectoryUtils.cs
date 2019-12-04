87744b9235d43f314dd9fd3e98d76489e74f6e58
Tyler <Tp229736@falmouth.ac.uk>
2019-12-04 10:43:56 +0000

    Finished Worksheet, Struggled on the final function of GetFilesOfSize.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,37 +24,81 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
-			throw new NotImplementedException();
+            long size = 0;
+            string[] files = Directory.GetFiles(directory, ".", SearchOption.AllDirectories);
+            foreach (string file in files) 
+            {
+                size += GetFileSize(file);
+            }
+            return size;
 		}
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
+            string[] files = Directory.GetFiles(directory, ".", SearchOption.AllDirectories);
+            return files.Length;
+        }
+
+        // Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
+        public static int GetDepth(string directory)
+        {
+            int DirDepth = 0;
+            string depth = Directory.GetDirectories(directory);
+            foreach (string Dir in depth) 
+            {
+                DirDepth++;
+            }
+            return DirDepth;
 		}
 
-		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
-		public static int GetDepth(string directory)
+        // Get the path and size (in bytes) of the smallest file below the given directory
+        public static Tuple<string, long> GetSmallestFile(string directory)
+        {
+            string smallestFile = "";
+            string[] filePaths = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+            foreach (string q in filePaths)
+            {
+                if (smallestFile == "")
+                {
+                    smallestFile = q;
+                }
+                else if (new FileInfo(smallestFile).Length > new FileInfo(q).Length)
+                {
+                    smallestFile = q;
+                }
+            }
+            return new Tuple<string, long>(smallestFile, new FileInfo(smallestFile).Length);
+        }
+            // Get the path and size (in bytes) of the largest file below the given directory
+            public static Tuple<string, long> GetLargestFile(string directory)
+        {
+            string largestFile = "";
+            string[] filePaths = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+            foreach (string q in filePaths)
+            {
+                if (largestFile == "")
+                {
+                    largestFile = q;
+                }
+                else if (new FileInfo(largestFile).Length < new FileInfo(q).Length)
+                {
+                    largestFile = q;
+                }
+            }
+            return new Tuple<string, long>(largestFile, new FileInfo(largestFile).Length);
+        }
+        // Get all files whose size is equal to the given value (in bytes) below the given directory
+        public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
-		}
-
-		// Get the path and size (in bytes) of the smallest file below the given directory
-		public static Tuple<string, long> GetSmallestFile(string directory)
-		{
-			throw new NotImplementedException();
-		}
-
-		// Get the path and size (in bytes) of the largest file below the given directory
-		public static Tuple<string, long> GetLargestFile(string directory)
-		{
-			throw new NotImplementedException();
-		}
-
-		// Get all files whose size is equal to the given value (in bytes) below the given directory
-		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
-		{
-			throw new NotImplementedException();
-		}
+            string[] filePaths = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+            foreach (string q in filePaths)
+            {
+                if (new FileInfo(q).Length == size)
+                {
+                    yield return q;
+                }
+            }
+        }
 	}
 }
